ca42a69fce68c0f28516bb88b9f6374718ac0fb7
Stine Griep <57923622+stigri@users.noreply.github.com>
2021-07-12 15:09:21 -0700

    Add files via upload

--- coinbias.py
+++ coinbias.py
@@ -6,7 +6,7 @@ from scipy import stats
 # define the bias of a weighted coin, unbiased = 0.5, biased > 0.5 or biased < 0.5
 bias = 0.7
 # number of coin flips
-n_flips = 100
+n_flips = 10000
 # array with range of possible biases
 range_bias = np.arange(0, 1.01, 0.01)
 # print(range_bias)
@@ -42,32 +42,26 @@ if p_value < alpha:
 else:
     print('h0 accepted')
 
-
 # calculate prior, likelihood and evidence for all flips
 for n in range(n_flips):
     p_prior = p_bias
     likelihood = np.multiply(np.power(range_bias, flips[n]), np.power(1 - range_bias, 1 - flips[n]))
     evidence = sum(np.multiply(likelihood, p_prior))
 
-# TODO: insert function to store images for animation
-
 # calculate posterior
     posterior = np.divide(np.multiply(likelihood, p_prior), evidence)
-
     p_bias = posterior
-
-
 
 # create figure dynamically
 fig = plt.figure()
 plt.plot(range_bias, p_bias)
-plt.suptitle('Bayes Model to identify Bias of Coin')
+plt.suptitle('')
 if p_value > alpha:
     plt.title('Number of Flips = {0}, P-Value = {1:1.4f}'.format(n_flips, p_value))
 else:
     plt.title('Number of Flips = {0}, P-Value < 0.01'.format(n_flips, p_value))
 
-plt.xlabel('Bias (unbiased = 0.5)')
+plt.xlabel('Bias = 0.7 (unbiased = 0.5)')
 plt.ylabel('P(Bias | Number of Flips)')
 plt.xlim(0, 1)
 plt.ylim(0, 1)
